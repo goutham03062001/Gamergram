8c8dac49c1200188ce775be1ab43942ff9299075
goutham03062001 <66135872+goutham03062001@users.noreply.github.com>
2021-11-28 17:32:21 +0530

    Update Navbar.js

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -101,7 +101,7 @@ const Navbar = () => {
     return (
         <nav>
             <div className="nav-wrapper">
-                <Link to={state ? '/' : '/login'} onClick={handleHome} className="brand-logo left logo">Gamergram</Link>
+                <Link to={state ? '/' : '/login'} onClick={handleHome} className="brand-logo left logo">ArtGalleryExpo</Link>
                 <div onClick={dropDownHandler} className="settings-icon">
                     <li></li>
                     <li></li>
